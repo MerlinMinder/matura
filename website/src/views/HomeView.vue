9c8a437617ca6e871931cb5690f71e75bbf4cca1
MerlinMinder <merlin.coding@gmail.com>
2022-07-08 10:10:13 +0200

    text for disposition

--- website/src/views/HomeView.vue
+++ website/src/views/HomeView.vue
@@ -1,11 +1,45 @@
 <template>
-<div class="container">
-  <div id="title">
-    <h1>PROGRESSFIT</h1>
-    <h2>Workout Progression App</h2>
-    <h3>Matura Work 2022</h3>
-    <h4>By Merlin Minder 3eW</h4>
-  </div>
+  <div class="container">
+    <div id="title">
+      <h1>PROGRESSFIT</h1>
+      <h2>Workout Progression App</h2>
+      <h3>Matura Work 2022</h3>
+      <h4>By Merlin Minder 3eW</h4>
+    </div>
+    <div id="disposition">
+      <p>Disposition:</p>
+      <p>
+        This Matura Work is a combination of a project and the correlating
+        documentation thereof. As seen in the title the project consists of a
+        fitness app which will be programmed from scratch. The resulting app and
+        code is open-sourced and available on Github. The documentation includes
+        the process from designing to programming, debugging and testing. It,
+        combined with further parts of the Matura Work are all on this website.
+        Disclaimer: Everything on this website is still subject to change untill
+        the final Deadline.
+      </p>
+      <p>The main question I will aim to answer is:</p>
+      <p>How does the process of programming a fitness app look like?</p>
+    </div>
+    <div id="timeline">
+      <p>Project timeline as of 08.07.2022:</p>
+      <p>11.07.2022 - 21.08.2022 Idea gathering mostly focused on design</p>
+      <p>22.08.2022 - 11.09.2022 Designing app on Figma</p>
+      <p>12.09.2022 - 23.10.2022 Programming the UI</p>
+      <p>24.10.2022 - 13.11.2022 Programming the functionalaties</p>
+      <p>14.11.2022 - 27.11.2022 Debugging and testing</p>
+      <p>28.11.2022 Finalisation of the project</p>
+    </div>
+    <div id="utensils">
+      <p>The technologies used in the project:</p>
+      <p>Figma (design)</p>
+      <p>VS Code (IDE)</p>
+      <p>Firebase (website hosting)</p>
+      <p>Vue.js (website framework)</p>
+      <p>React Native (App framework)</p>
+      <p>Google (Research)</p>
+      <p>Github (Code sharing)</p>
+    </div>
   </div>
 </template>
 
@@ -13,7 +47,7 @@
 
 <style lang="scss" scoped>
 .container {
-    width: "100%";
+  width: "100%";
 }
 
 #title {

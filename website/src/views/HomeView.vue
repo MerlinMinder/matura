978e55560320b39e5579ac85bf2295cfd82b8bd1
MerlinMinder <merlin.coding@gmail.com>
2022-07-25 16:35:42 +0200

    added calendar (hardcoded)

--- website/src/views/HomeView.vue
+++ website/src/views/HomeView.vue
@@ -31,16 +31,26 @@
       </p>
     </div>
     <div id="timeline">
-      <p id="timeline-title">Project Timeline as of 21.07.2022</p>
+      <p id="timeline-title">Project Timeline as of 25.07.2022</p>
       <div id="timeline-parts">
         <p>08.07.2022 Kickoff of the project</p>
         <p>11.07.2022 - 18.07.2022 Website creation</p>
         <p>19.07.2022 - 21.08.2022 Design idea gathering + website styling</p>
         <p>22.08.2022 - 11.09.2022 Designing app on Figma</p>
-        <p>12.09.2022 - 23.10.2022 Programming the UI</p>
-        <p>24.10.2022 - 13.11.2022 Programming the functionalities</p>
+        <p>12.09.2022 - 16.10.2022 Programming the UI</p>
+        <p>17.10.2022 - 13.11.2022 Programming the functionalities</p>
         <p>14.11.2022 - 27.11.2022 Debugging and testing</p>
         <p>28.11.2022 Finalisation of the project</p>
+        <p>05.12.2022 Official deadline</p>
+      </div>
+    </div>
+    <div id="calendar">
+      <div id="month" v-for="month in calendar">
+        <div id="day-container" v-for="day in month">
+          <div id="day" :style="{ 'background-color': day.type }">
+            {{ day.day }}
+          </div>
+        </div>
       </div>
     </div>
     <div id="utensils">
@@ -87,6 +97,218 @@ const workingtime = ref({
   posts: 0,
 });
 
+const types = {
+  none: "gray",
+  kickoff: "#95FF42",
+  website: "#FB42FF",
+  idea: "#FFB442",
+  design: "#42FFFF",
+  ui: "#42FF55",
+  func: "#424AFF",
+  debug: "#A142FF",
+  finalasiation: "#FF4242",
+  deadline: "black",
+};
+
+const calendar = [
+  [
+    { day: 1, type: types.none },
+    { day: 2, type: types.none },
+    { day: 3, type: types.none },
+    { day: 4, type: types.none },
+    { day: 5, type: types.none },
+    { day: 6, type: types.none },
+    { day: 7, type: types.none },
+    { day: 8, type: types.kickoff },
+    { day: 9, type: types.website },
+    { day: 10, type: types.website },
+    { day: 11, type: types.website },
+    { day: 12, type: types.website },
+    { day: 13, type: types.website },
+    { day: 14, type: types.website },
+    { day: 15, type: types.website },
+    { day: 16, type: types.website },
+    { day: 17, type: types.website },
+    { day: 18, type: types.website },
+    { day: 19, type: types.idea },
+    { day: 20, type: types.idea },
+    { day: 21, type: types.idea },
+    { day: 22, type: types.idea },
+    { day: 23, type: types.idea },
+    { day: 24, type: types.idea },
+    { day: 25, type: types.idea },
+    { day: 26, type: types.idea },
+    { day: 27, type: types.idea },
+    { day: 28, type: types.idea },
+    { day: 29, type: types.idea },
+    { day: 30, type: types.idea },
+    { day: 31, type: types.idea },
+  ],
+  [
+    { day: 1, type: types.idea },
+    { day: 2, type: types.idea },
+    { day: 3, type: types.idea },
+    { day: 4, type: types.idea },
+    { day: 5, type: types.idea },
+    { day: 6, type: types.idea },
+    { day: 7, type: types.idea },
+    { day: 8, type: types.idea },
+    { day: 9, type: types.idea },
+    { day: 10, type: types.idea },
+    { day: 11, type: types.idea },
+    { day: 12, type: types.idea },
+    { day: 13, type: types.idea },
+    { day: 14, type: types.idea },
+    { day: 15, type: types.idea },
+    { day: 16, type: types.idea },
+    { day: 17, type: types.idea },
+    { day: 18, type: types.idea },
+    { day: 19, type: types.idea },
+    { day: 20, type: types.idea },
+    { day: 21, type: types.idea },
+    { day: 22, type: types.design },
+    { day: 23, type: types.design },
+    { day: 24, type: types.design },
+    { day: 25, type: types.design },
+    { day: 26, type: types.design },
+    { day: 27, type: types.design },
+    { day: 28, type: types.design },
+    { day: 29, type: types.design },
+    { day: 30, type: types.design },
+    { day: 31, type: types.design },
+  ],
+  [
+    { day: 1, type: types.design },
+    { day: 2, type: types.design },
+    { day: 3, type: types.design },
+    { day: 4, type: types.design },
+    { day: 5, type: types.design },
+    { day: 6, type: types.design },
+    { day: 7, type: types.design },
+    { day: 8, type: types.design },
+    { day: 9, type: types.design },
+    { day: 10, type: types.design },
+    { day: 11, type: types.design },
+    { day: 12, type: types.ui },
+    { day: 13, type: types.ui },
+    { day: 14, type: types.ui },
+    { day: 15, type: types.ui },
+    { day: 16, type: types.ui },
+    { day: 17, type: types.ui },
+    { day: 18, type: types.ui },
+    { day: 19, type: types.ui },
+    { day: 20, type: types.ui },
+    { day: 21, type: types.ui },
+    { day: 22, type: types.ui },
+    { day: 23, type: types.ui },
+    { day: 24, type: types.ui },
+    { day: 25, type: types.ui },
+    { day: 26, type: types.ui },
+    { day: 27, type: types.ui },
+    { day: 28, type: types.ui },
+    { day: 29, type: types.ui },
+    { day: 30, type: types.ui },
+  ],
+  [
+    { day: 1, type: types.ui },
+    { day: 2, type: types.ui },
+    { day: 3, type: types.ui },
+    { day: 4, type: types.ui },
+    { day: 5, type: types.ui },
+    { day: 6, type: types.ui },
+    { day: 7, type: types.ui },
+    { day: 8, type: types.ui },
+    { day: 9, type: types.ui },
+    { day: 10, type: types.ui },
+    { day: 11, type: types.ui },
+    { day: 12, type: types.ui },
+    { day: 13, type: types.ui },
+    { day: 14, type: types.ui },
+    { day: 15, type: types.ui },
+    { day: 16, type: types.ui },
+    { day: 17, type: types.func },
+    { day: 18, type: types.func },
+    { day: 19, type: types.func },
+    { day: 20, type: types.func },
+    { day: 21, type: types.func },
+    { day: 22, type: types.func },
+    { day: 23, type: types.func },
+    { day: 24, type: types.func },
+    { day: 25, type: types.func },
+    { day: 26, type: types.func },
+    { day: 27, type: types.func },
+    { day: 28, type: types.func },
+    { day: 29, type: types.func },
+    { day: 30, type: types.func },
+    { day: 31, type: types.func },
+  ],
+  [
+    { day: 1, type: types.func },
+    { day: 2, type: types.func },
+    { day: 3, type: types.func },
+    { day: 4, type: types.func },
+    { day: 5, type: types.func },
+    { day: 6, type: types.func },
+    { day: 7, type: types.func },
+    { day: 8, type: types.func },
+    { day: 9, type: types.func },
+    { day: 10, type: types.func },
+    { day: 11, type: types.func },
+    { day: 12, type: types.func },
+    { day: 13, type: types.func },
+    { day: 14, type: types.debug },
+    { day: 15, type: types.debug },
+    { day: 16, type: types.debug },
+    { day: 17, type: types.debug },
+    { day: 18, type: types.debug },
+    { day: 19, type: types.debug },
+    { day: 20, type: types.debug },
+    { day: 21, type: types.debug },
+    { day: 22, type: types.debug },
+    { day: 23, type: types.debug },
+    { day: 24, type: types.debug },
+    { day: 25, type: types.debug },
+    { day: 26, type: types.debug },
+    { day: 27, type: types.debug },
+    { day: 28, type: types.finalasiation },
+    { day: 29, type: types.none },
+    { day: 30, type: types.none },
+  ],
+  [
+    { day: 1, type: types.none },
+    { day: 2, type: types.none },
+    { day: 3, type: types.none },
+    { day: 4, type: types.none },
+    { day: 5, type: types.deadline },
+    { day: 6, type: types.none },
+    { day: 7, type: types.none },
+    { day: 8, type: types.none },
+    { day: 9, type: types.none },
+    { day: 10, type: types.none },
+    { day: 11, type: types.none },
+    { day: 12, type: types.none },
+    { day: 13, type: types.none },
+    { day: 14, type: types.none },
+    { day: 15, type: types.none },
+    { day: 16, type: types.none },
+    { day: 17, type: types.none },
+    { day: 18, type: types.none },
+    { day: 19, type: types.none },
+    { day: 20, type: types.none },
+    { day: 21, type: types.none },
+    { day: 22, type: types.none },
+    { day: 23, type: types.none },
+    { day: 24, type: types.none },
+    { day: 25, type: types.none },
+    { day: 26, type: types.none },
+    { day: 27, type: types.none },
+    { day: 28, type: types.none },
+    { day: 29, type: types.none },
+    { day: 30, type: types.none },
+    { day: 31, type: types.none },
+  ],
+];
+
 onMounted(async () => {
   const postsdata = await getDocs(collection(db, "posts"));
   postsdata.forEach((doc) => {
@@ -101,7 +323,6 @@ onMounted(async () => {
       workingtime.value.debugging += day.debugging;
       workingtime.value.posts += day.posts;
     });
-  totaldays.splice(92, 1);
 });
 </script>
 
@@ -249,21 +470,33 @@ onMounted(async () => {
   display: flex;
   flex-direction: row;
   margin-bottom: 10vh;
+  flex-wrap: wrap;
+  width: 60vw;
+  margin-left: 10vw;
+  justify-content: space-evenly;
 
   #month {
-    border: solid white 1px;
-    width: max-content;
+    border-radius: 10px;
+    padding: 20px;
+    margin: 50px 0;
+    width: 235px;
     height: fit-content;
     display: flex;
     flex-wrap: wrap;
     justify-content: flex-start;
+    box-shadow: inset 6px 6px 8px rgba(153, 153, 153, 0.5),
+      inset -6px -6px 8px rgba(0, 0, 0, 0.5);
 
     #day {
       height: 30px;
       width: 30px;
-      margin-right: 1px;
-      border: solid black 1px;
-      background-color: #42ffff;
+      margin: 1.5px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 5px;
+      box-shadow: inset 2px 2px 3px rgba(0, 0, 0, 0.5),
+        inset -2px -2px 3px rgba(230, 230, 230, 0.5);
     }
   }
 }
